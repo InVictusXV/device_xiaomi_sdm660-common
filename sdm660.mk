57a1b29648851e79b1dc32b38706b139b50bd4e3
sbwml <984419930@qq.com>
2020-12-10 13:49:03 +0800

    sdm660-common: bump vendor.display.config to 2.0
    * New WFD blobs need vendor.display.confi@2.0.so

--- sdm660.mk
+++ sdm660.mk
@@ -219,8 +219,8 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libqdMetaData.system \
     libtinyxml \
-    vendor.display.config@1.9 \
-    vendor.display.config@1.9.vendor
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor
 
 # Density
 TARGET_SCREEN_DENSITY := 400
